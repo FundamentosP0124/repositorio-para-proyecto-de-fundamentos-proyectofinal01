bcef17e595dda033277de98b9be2d2204193ab4c
Yuliana <128511401+YulianaDeras@users.noreply.github.com>
2024-06-20 17:32:36 -0600

    "feat: ✨ funcion para crear archivos con lista de participantes"

--- proyectofinal.cpp
+++ proyectofinal.cpp
@@ -160,3 +160,12 @@ void listarParticipantes() {
         cout << i + 1 << ". " << participantes[i].nombre << " - Ganadas: " << participantes[i].partidas_ganadas << ", Perdidas: " << participantes[i].partidas_perdidas << "\n";
     }
 }
+
+void guardarParticipantesEnArchivo() {
+    // Guardar la información de los participantes en un archivo
+    ofstream archivo("participantes.txt");
+    for (int i = 0; i < numero_participantes; ++i) {
+        archivo << participantes[i].nombre << " " << participantes[i].partidas_ganadas << " " << participantes[i].partidas_perdidas << "\n";
+    }
+    archivo.close();
+}
