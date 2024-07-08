27454748b9be7f78851a8f30c543938a47230597
faxhoop11 <164411445+faxhoop11@users.noreply.github.com>
2024-07-08 17:05:04 -0600

    "feat: Función principal del juego TicTacToeVsPC"

--- logicadeljuego.cpp
+++ logicadeljuego.cpp
@@ -63,3 +63,10 @@ void iniciarJuegoTicTacToe() {
         }
     }
 }
+void iniciarJuegoTicTacToeVsPC() {
+    // Tablero del juego Tic Tac Toe
+    char tablero[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+    char jugadorActual = 'X'; // El primer jugador usa 'X'
+    int movimiento;
+    bool juegoTerminado = false;
+    string nombreJugador;
