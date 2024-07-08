bb669802d5c801e59cfb69e6d71086ab270c7d67
Prisleiva11 <164081051+Prisleiva11@users.noreply.github.com>
2024-07-08 17:50:30 -0600

    "feat: Implementar la funcion dibujar tablero"

--- Tableros.cpp
+++ Tableros.cpp
@@ -2,3 +2,14 @@ char tablero[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
 
 // Función para dibujar el tablero del juego Tic Tac Toe
 void dibujarTablero(char tablero[3][3]) {
+ cout << "\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << " " << tablero[i][j];
+            if (j < 2) cout << " |";
+        }
+        cout << "\n";
+        if (i < 2) cout << "---|---|---\n";
+    }
+    cout << "\n";
+}
