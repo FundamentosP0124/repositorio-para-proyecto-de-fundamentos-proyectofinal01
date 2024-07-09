a68c7d775a64df85bb5782c95922751d6689f46b
Raquel-0105 <164411359+Raquel-0105@users.noreply.github.com>
2024-07-08 18:21:45 -0600

    "feat: Implementación de la variable inicializar puntuaciones"

--- puntuaciones.cpp
+++ puntuaciones.cpp
@@ -1,1 +1,8 @@
 void inicializarPuntuaciones() {
+// Inicializar las puntuaciones de los participantes
+    for (int i = 0; i < numero_participantes; ++i) {
+        participantes[i].partidas_ganadas = 0;
+        participantes[i].partidas_perdidas = 0;
+    }
+    guardarPuntuacionesEnArchivo();
+}
